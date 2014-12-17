82bf15f1615b6c29885e86eb5351f262d4fd50ac
David Molineus <mail@netzmacht.de>
2014-12-17 09:14:38 +0100

    Rename abstract extractor.

--- src/AuthorExtractor/GitAuthorExtractor.php
+++ src/AuthorExtractor/GitAuthorExtractor.php
@@ -27,7 +27,7 @@ use Symfony\Component\Finder\Finder;
 /**
  * Extract the author information from a git repository.
  */
-class GitAuthorExtractor extends AbstractGitExtractor
+class GitAuthorExtractor extends AbstractGitAuthorExtractor
 {
     /**
      * Optional attached finder for processing multiple files.
