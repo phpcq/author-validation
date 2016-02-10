ed37c1bdf2d05b1f3876fa5bada4ba053df50773
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-02-11 00:03:15 +0100

    Correct file headers including author information

--- src/Json/JsonFormatter.php
+++ src/Json/JsonFormatter.php
@@ -15,9 +15,9 @@
  * @author     Jordi Boggiano <j.boggiano@seld.be>
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Tristan Lins <tristan@lins.io>
- * @copyright  Christian Schiffler <c.schiffler@cyberspectrum.de>, Tristan Lins <tristan@lins.io>
- * @link       https://github.com/phpcq/author-validation
+ * @copyright  2014-2016 Christian Schiffler <c.schiffler@cyberspectrum.de>, Tristan Lins <tristan@lins.io>
  * @license    https://github.com/phpcq/author-validation/blob/master/LICENSE MIT
+ * @link       https://github.com/phpcq/author-validation
  * @filesource
  */
 
