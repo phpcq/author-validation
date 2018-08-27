57714a68a67103410c584a7db9842d0d0ece6cc9
Sven Baumann <baumann.sv@gmail.com>
2018-08-27 12:36:16 +0200

    Add deprecation notice for replaced abstract class

---
+++ src/AuthorExtractor/JsonAuthorExtractor.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of phpcq/author-validation.
+ *
+ * (c) 2014-2018 Christian Schiffler, Tristan Lins
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    phpcq/author-validation
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Tristan Lins <tristan@lins.io>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2014-2018 Christian Schiffler <c.schiffler@cyberspectrum.de>, Tristan Lins <tristan@lins.io>
+ * @license    https://github.com/phpcq/author-validation/blob/master/LICENSE MIT
+ * @link       https://github.com/phpcq/author-validation
+ * @filesource
+ */
+
+namespace PhpCodeQuality\AuthorValidation\AuthorExtractor;
+
+use PhpCodeQuality\AuthorValidation\AuthorExtractor;
+use PhpCodeQuality\AuthorValidation\PatchingExtractor;
+
+/**
+ * Abstract class for author extraction.
+ *
+ * @deprecated This class is deprecated since 1.2 and where removed in 2.0.
+ *             Use the trait AuthorExtractorTrait and JsonAuthorExtractorTrait.
+ */
+abstract class JsonAuthorExtractor implements AuthorExtractor, PatchingExtractor
+{
+    use AuthorExtractorTrait;
+    use JsonAuthorExtractorTrait;
+}
